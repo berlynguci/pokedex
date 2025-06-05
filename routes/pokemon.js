30795e3e03e4637d80fddb03a4493a7198e73fe5
BabyBokChoy8 <berlinsanity101@gmail.com>
2025-06-05 16:33:59 +0800

    pokedex

---
+++ routes/pokemon.js
@@ -0,0 +1,34 @@
+import express from "express";
+import axios from "axios";
+import env from "dotenv";
+
+env.config();
+
+const router = express.Router();
+const API_URL = process.env.API_URL;
+
+router.get("/", async (req, res) => {
+  try {
+    ("");
+    const response = await axios.get(
+      "https://pokeapi.co/api/v2/pokemon?limit=50"
+    );
+    console.log("went");
+
+    res.render("pages/home", { pokemonList: response.data.results });
+  } catch (error) {
+    res.status(500).send("Error fetching Pokémon data");
+  }
+});
+
+router.get("/pokemon/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    res.render("pages/details", { pokemon: response.data });
+  } catch (error) {
+    res.status(500).send("Error fetching Pokémon data");
+  }
+});
+
+export default router;
